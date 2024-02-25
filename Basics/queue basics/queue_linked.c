9201fa91306756912eb05e05a41c926ab557d12a
Aditya Bhendavadekar <adityab1193@gmail.com>
2024-02-25 22:05:40 +0530

    Queue linked list

--- Basics/queue basics/queue_linked.c
+++ Basics/queue basics/queue_linked.c
@@ -6,37 +6,58 @@ struct queue{
     struct queue *next;
 };
 
-void display(struct queue *h){
+void display(struct queue *h) {
+    if (h == NULL) {
+        printf("Queue is empty.\n");
+        return;
+    }
 
     struct queue *temp = h;
 
-    while (temp!=NULL)
-    {
-        printf(" %d ", temp->data);
+    printf("Queue elements: ");
+    while (temp != NULL || temp->next==NULL) {
+        printf("%d ", temp->data);
         temp = temp->next;
     }
+    printf("\n");
+}
+
+void dequeue(struct queue **f, struct queue **h, struct queue **r){
+    struct queue *temp;
+    temp = *f;
+
+    if(temp==NULL){
+        printf("Queue underflow.\n");
+        return;
+    }
+
+    printf("Dequeued element: %d\n",temp->data);
+    
+    if (*f == *r) { // If there's only one element in the queue
+        *f = *r = NULL;
+    } else {
+        *f = (*f)->next; // Move front to the next element
+    }
+
+    free(temp);
 }
 
 void enqueue(struct queue **f, struct queue **h, struct queue **r ){
     struct queue *node = (struct queue*) malloc(sizeof(struct queue));
     node->next=NULL;
 
-    printf("Enter data insert: ");
+    printf("Enter data to insert: ");
     scanf("%d", &node->data);
 
-    if(*h==NULL){
+    if(*h==NULL && *f==NULL){
         *h = *f = *r = node;
     }
     else{
-        *r = node;
-        struct queue *tmp = *h;
-            while (tmp->next != NULL)
-            {
-                *r = tmp = tmp->next;
-            }
-            tmp->next = node;
+        (*r)->next = node; // Connect the current rear to the new node
+        *r = node; // Update the rear pointer to the new node
     }
 }
+
 
 int main(){
 
@@ -55,7 +76,7 @@ do{
             enqueue(&front, &head,&rear);
             break;
         case 2:
-            // dequeue(&front, head, &rear);
+            dequeue(&front, &head, &rear);
             break;
         case 3:
             display(head);
@@ -67,6 +88,9 @@ do{
             printf("Enter correct option please.");
 
     }
+    // printf("Front pointing to: %d", front->data);
+    // printf("Rear pointing to: %d", head->data);
+
 }while(option != 4); 
 
     return 0;
